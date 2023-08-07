a90a76ef6ea65067b9081ca5e13495eb79022fed
Tatiana Maslenkova <tsevastianova21@gmail.com>
2023-08-07 17:18:04 +0300

    Homework_6

--- Homework_6/__init__.py
+++ Homework_6/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ["Chess_task", "homework6_task1", "homework6_task2", "homework6_task3", "homework6_task4"]
+__all__ = ["homework6_task1", "homework6_task2", "homework6_task3", "homework6_task4", "Chess_task"]
--- Homework_6/homework6_task4.py
+++ Homework_6/homework6_task4.py
@@ -1,9 +1,9 @@
+from Chess_task import good_position, NEED_GOOD_POSITIONS
+
 """
 Напишите функцию в шахматный модуль. Используйте генератор случайных чисел для случайной расстановки ферзей в задаче выше.
 Проверяйте различный случайные  варианты и выведите 4 успешных расстановки.
 """
-
-from Chess_task import good_position, NEED_GOOD_POSITIONS
 
 print("4 успешные расстановки 8 ферзей: ")
 good_position(NEED_GOOD_POSITIONS)
